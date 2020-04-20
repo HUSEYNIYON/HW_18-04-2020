503b2972b0e5164b5fa3e8a9b0d7963313bee9e6
Huseyn <your>
2020-04-20 13:29:14 +0300

    Maked four static methods for Math-operation

--- HW_18-04-2020/HW_18-04-2020/Calculator.cs
+++ HW_18-04-2020/HW_18-04-2020/Calculator.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace HW_18_04_2020
 {
-    class Calculator
+    class Calculator<T>
     {
+        public static T SUM<T>(T Gen1, T Gen2)
+        {
+
+        }
+        public static T MIN<T>(T Gen1, T Gen2)
+        {
+
+        }
+        public static T MUL<T>(T Gen1, T Gen2)
+        {
+
+        }
+        public static T Div<T>(T Gen1, T Gen2)
+        {
+            
+        }
     }
 }
